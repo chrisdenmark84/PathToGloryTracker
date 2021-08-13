1d4c085c2eebec2c2c7545081fe59d193f182555
Chris Denmark <chrisdanish@googlemail.com>
2021-08-14 00:04:54 +0100

    fix #1 Amend route with create above show

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,8 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+| 
+|route get c/create needs to be above /c/{campagin}. I'm not sure why
 */
 
 Route::get('/', function () {
